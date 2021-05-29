5377c569dbd7feaacaf8e9327dd15cd4633fe65b
mylifem807 <84409836+mylifem807@users.noreply.github.com>
2021-05-29 19:34:44 +0530

    Update __main__.py

--- AlexaSongBot/__main__.py
+++ AlexaSongBot/__main__.py
@@ -11,7 +11,7 @@ from AlexaSongBot.sql.chat_sql import add_chat_to_db
 
 start_text = """
 Hey [{}](tg://user?id={}),
-I'm Alexa 🤗
+I'm Rosy 🤗
 Just send me the song name you want to download.
 Eg: ```/song Faded Alan Walker```
 """
@@ -35,7 +35,7 @@ async def start(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        text="🔳 Source Code 🔳", url="https://github.com/Mr-Dark-Prince/AlexaSongBot"
+                        text="🔳 Source Code 🔳", url="https://github.com/mylifem807/AlexaSongBot"
                     )
                 ]
             ]
