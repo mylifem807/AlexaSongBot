46e02d796486205810d6195ed15fdf7e01e74810
mylifem807 <84409836+mylifem807@users.noreply.github.com>
2021-06-01 14:43:08 +0530

    Update __main__.py

--- AlexaSongBot/__main__.py
+++ AlexaSongBot/__main__.py
@@ -1,4 +1,4 @@
-# © @Mr_Dark_Prince
+# © @ayyanar123
 from config import OWNER_ID
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from AlexaSongBot.modules import *
@@ -13,7 +13,7 @@ start_text = """
 Hey [{}](tg://user?id={}),
 I'm Alexa 🤗
 Just send me the song name you want to download.
-Eg: ```/song Faded Alan Walker```
+Eg: ```/song Unnale Ennalum En Jeevan```
 """
 
 owner_help = """
